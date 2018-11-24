3ed99f45b1812941af1f568fc9d0975a5681d56e
marc <marc@rromana.com>
2018-11-24 10:41:30 -0800

    test

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/saidur/pen/kgHDu.
+
+ Like facebook. 
---
+++ js/index.js
@@ -0,0 +1,29 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('player', {
+    playerVars: {
+      'autoplay': 1, 
+      'controls': 0,
+      'autohide': 1,
+      'loop': 1,
+      'wmode':'opaque' },
+    videoId: 'R_7Mo8oGP4E',
+    events: {
+      'onReady': onPlayerReady}
+  });
+}
+
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+ event.target.unmute();
+  //event.target.setPlaybackRate(0.5);
+}
