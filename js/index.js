0abaceddaa6c811f5bb8a4c77493d480ff5ddc95
marc <marc@rromana.com>
2019-03-07 18:19:07 -0800

    test

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/miromannino/Justified-Gallery
-const size = 500;
-
-$('#justified-gallery').justifiedGallery({
-  lastRow: 'justify',
-  rowHeight: size,
-  maxRowHeight: size,
-  margins: 0
-})
