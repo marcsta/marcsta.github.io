f9896d988a82771c23cd4ae88793b77f99073c4c
marc <marc@rromana.com>
2020-06-19 11:21:14 -0700

    test

--- js/demo.js
+++ js/demo.js
@@ -1,264 +1,255 @@
 /**
-* demo.js
-* http://www.codrops.com
-*
-* Licensed under the MIT license.
-* http://www.opensource.org/licenses/mit-license.php
-* 
-* Copyright 2019, Codrops
-* http://www.codrops.com
-*/
+ * demo.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
 {
-    // helper functions
-    const MathUtils = {
-        // map number x from range [a, b] to [c, d]
-        map: (x, a, b, c, d) => (x - a) * (d - c) / (b - a) + c,
-        // linear interpolation
-        lerp: (a, b, n) => (1 - n) * a + n * b,
-        // Random float
-        getRandomFloat: (min, max) => (Math.random() * (max - min) + min).toFixed(2)
-    };
+	// from http://www.quirksmode.org/js/events_properties.html#position
+	const getMousePos = (e) => {
+		let posx = 0;
+		let posy = 0;
+		if (!e) {let e = window.event};
+		if (e.pageX || e.pageY) 	{
+			posx = e.pageX;
+			posy = e.pageY;
+		}
+		else if (e.clientX || e.clientY) 	{
+			posx = e.clientX + document.body.scrollLeft
+				+ document.documentElement.scrollLeft;
+			posy = e.clientY + document.body.scrollTop
+				+ document.documentElement.scrollTop;
+		}
+		return {
+			x : posx,
+			y : posy
+		};
+	}
 
-    // body element
-    const body = document.body;
-    
-    // calculate the viewport size
-    let winsize;
-    const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
-    calcWinsize();
-    // and recalculate on resize
-    window.addEventListener('resize', calcWinsize);
-    
-    // scroll position
-    let docScroll;
-    // for scroll speed calculation
-    let lastScroll;
-    let scrollingSpeed = 0;
-    // scroll position update function
-    const getPageYScroll = () => docScroll = window.pageYOffset || document.documentElement.scrollTop;
-    window.addEventListener('scroll', getPageYScroll);
+	// From https://davidwalsh.name/javascript-debounce-function.
+	const debounce = (func, wait, immediate) => {
+		let timeout;
+		return () => {
+			let context = this, args = arguments;
+			let later = () => {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			};
+			let callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		};
+	};
 
-    // Item
-    class Item {
-        constructor(el) {
-            // the .item element
-            this.DOM = {el: el};
-            // the inner image
-            this.DOM.image = this.DOM.el.querySelector('.content__item-img');
-            this.DOM.imageWrapper = this.DOM.image.parentNode;
-            this.DOM.title = this.DOM.el.querySelector('.content__item-title');
-            this.renderedStyles = {
-                // here we define which property will change as we scroll the page and the item is inside the viewport
-                // in this case we will be:
-                // - scaling the inner image
-                // - translating the item's title
-                // we interpolate between the previous and current value to achieve a smooth effect
-                imageScale: {
-                    // interpolated value
-                    previous: 0, 
-                    // current value
-                    current: 0, 
-                    // amount to interpolate
-                    ease: 0.1,
-                    // current value setter
-                    setValue: () => {
-                        const toValue = 1.5;
-                        const fromValue = 1;
-                        const val = MathUtils.map(this.props.top - docScroll, winsize.height, -1 * this.props.height, fromValue, toValue);
-                        return Math.max(Math.min(val, toValue), fromValue);
-                    }
-                },
-                titleTranslationY: {
-                    previous: 0, 
-                    current: 0, 
-                    ease: 0.1,
-                    fromValue: Number(MathUtils.getRandomFloat(30,400)),
-                    setValue: () => {
-                        const fromValue = this.renderedStyles.titleTranslationY.fromValue;
-                        const toValue = -1*fromValue;
-                        const val = MathUtils.map(this.props.top - docScroll, winsize.height, -1 * this.props.height, fromValue, toValue);
-                        return fromValue < 0 ? Math.min(Math.max(val, fromValue), toValue) : Math.max(Math.min(val, fromValue), toValue);
-                    }
-                }
-            };
-            // gets the item's height and top (relative to the document)
-            this.getSize();
-            // set the initial values
-            this.update();
-            // use the IntersectionObserver API to check when the element is inside the viewport
-            // only then the element styles will be updated
-            this.observer = new IntersectionObserver((entries) => {
-                entries.forEach(entry => this.isVisible = entry.intersectionRatio > 0);
-            });
-            this.observer.observe(this.DOM.el);
-            // init/bind events
-            this.initEvents();
-        }
-        update() {
-            // sets the initial value (no interpolation)
-            for (const key in this.renderedStyles ) {
-                this.renderedStyles[key].current = this.renderedStyles[key].previous = this.renderedStyles[key].setValue();
-            }
-            // apply changes/styles
-            this.layout();
-        }
-        getSize() {
-            const rect = this.DOM.el.getBoundingClientRect();
-            this.props = {
-                // item's height
-                height: rect.height,
-                // offset top relative to the document
-                top: docScroll + rect.top
-            }
-        }
-        initEvents() {
-            window.addEventListener('resize', () => this.resize());
-        }
-        resize() {
-            // gets the item's height and top (relative to the document)
-            this.getSize();
-            // on resize reset sizes and update styles
-            this.update();
-        }
-        render() {
-            // update the current and interpolated values
-            for (const key in this.renderedStyles ) {
-                this.renderedStyles[key].current = this.renderedStyles[key].setValue();
-                this.renderedStyles[key].previous = MathUtils.lerp(this.renderedStyles[key].previous, this.renderedStyles[key].current, this.renderedStyles[key].ease);
-            }
-            
-            // and apply changes
-            this.layout();
-        }
-        layout() {
-            // scale the image
-            this.DOM.image.style.transform = `scale3d(${this.renderedStyles.imageScale.previous},${this.renderedStyles.imageScale.previous},1)`;
-            // translate the title
-            this.DOM.title.style.transform = `translate3d(0,${this.renderedStyles.titleTranslationY.previous}px,0)`;
-        }
-    }
+	const distance = (x1,x2,y1,y2) => {
+		const a = x1 - x2;
+		const b = y1 - y2;
+		return Math.sqrt(a*a + b*b);
+	};
 
-    // SmoothScroll
-    class SmoothScroll {
-        constructor() {
-            // the <main> element
-            this.DOM = {main: document.querySelector('main')};
-            // the scrollable element
-            // we translate this element when scrolling (y-axis)
-            this.DOM.scrollable = this.DOM.main.querySelector('div[data-scroll]');
-            // the items on the page
-            this.items = [];
-            this.DOM.content = this.DOM.main.querySelector('.content');
-            [...this.DOM.content.querySelectorAll('.content__item')].forEach(item => this.items.push(new Item(item)));
-            // here we define which property will change as we scroll the page
-            // in this case we will be translating on the y-axis
-            // we interpolate between the previous and current value to achieve the smooth scrolling effect
-            this.renderedStyles = {
-                translationY: {
-                    // interpolated value
-                    previous: 0, 
-                    // current value
-                    current: 0, 
-                    // amount to interpolate
-                    ease: 0.1,
-                    // current value setter
-                    // in this case the value of the translation will be the same like the document scroll
-                    setValue: () => docScroll
-                }
-            };
-            // set the body's height
-            this.setSize();
-            // set the initial values
-            this.update();
-            // the <main> element's style needs to be modified
-            this.style();
-            // init/bind events
-            this.initEvents();
-            // start the render loop
-            requestAnimationFrame(() => this.render());
-        }
-        update() {
-            // sets the initial value (no interpolation) - translate the scroll value
-            for (const key in this.renderedStyles ) {
-                this.renderedStyles[key].current = this.renderedStyles[key].previous = this.renderedStyles[key].setValue();   
-            }   
-            // translate the scrollable element
-            this.layout();
-        }
-        layout() {
-            this.DOM.scrollable.style.transform = `translate3d(0,${-1*this.renderedStyles.translationY.previous}px,0)`;
-        }
-        setSize() {
-            // set the heigh of the body in order to keep the scrollbar on the page
-            body.style.height = `${this.DOM.scrollable.scrollHeight}px`;
-        }
-        style() {
-            // the <main> needs to "stick" to the screen and not scroll
-            // for that we set it to position fixed and overflow hidden 
-            this.DOM.main.style.position = 'fixed';
-            this.DOM.main.style.width = this.DOM.main.style.height = '100%';
-            this.DOM.main.style.top = this.DOM.main.style.left = 0;
-            this.DOM.main.style.overflow = 'hidden';
-        }
-        initEvents() {
-            // on resize reset the body's height
-            window.addEventListener('resize', () => this.setSize());
-        }
-        render() {
-            // Get scrolling speed
-            // Update lastScroll
-            scrollingSpeed = Math.abs(docScroll - lastScroll);
-            lastScroll = docScroll;
-            
-            // update the current and interpolated values
-            for (const key in this.renderedStyles ) {
-                this.renderedStyles[key].current = this.renderedStyles[key].setValue();
-                this.renderedStyles[key].previous = MathUtils.lerp(this.renderedStyles[key].previous, this.renderedStyles[key].current, this.renderedStyles[key].ease);    
-            }
-            // and translate the scrollable element
-            this.layout();
-            
-            // for every item
-            for (const item of this.items) {
-                // if the item is inside the viewport call it's render function
-                // this will update item's styles, based on the document scroll value and the item's position on the viewport
-                if ( item.isVisible ) {
-                    if ( item.insideViewport ) {
-                        item.render();
-                    }
-                    else {
-                        item.insideViewport = true;
-                        item.update();
-                    }
-                }
-                else {
-                    item.insideViewport = false;
-                }
-            }
-            
-            // loop..
-            requestAnimationFrame(() => this.render());
-        }
-    }
+	let win = {width: window.innerWidth, height: window.innerHeight};
+	let center = {x: win.width/2, y: win.height/2};
 
-    /***********************************/
-    /********** Preload stuff **********/
+	class GridItem {
+		constructor(el, options) {
+			this.CONFIG = {
+				filledColor: '#fff'
+			};
+			Object.assign(this.CONFIG, options);
+			this.DOM = {};
+			this.DOM.el = el;
+			const bcr = this.DOM.el.getBoundingClientRect();
+			this.itemCenter = {
+				x: bcr.left + bcr.width/2,
+				y: bcr.top + bcr.height/2
+			};
+			this.revealer = new Revealer(this.DOM.el, {color: this.CONFIG.filledColor || window.getComputedStyle(document.body, null).backgroundColor});
+			this.initEvents();
+		}
+		initEvents() {
+			window.addEventListener('resize', (ev) => debounce(this.onresize()));
+		}
+		onresize(ev) {
+			const bcr = this.DOM.el.getBoundingClientRect();
+			this.itemCenter = {
+				x: bcr.left + bcr.width/2,
+				y: bcr.top + bcr.height/2
+			};
+		}
+		show(animation = true) {
+			return animation ? this.revealer.show({direction: this.DOM.el.dataset.direction || 'rtl', delay: this.DOM.el.dataset.delay || 0}) : this.revealer.show();
+		}
+		hide(animation = true) {
+			return animation ? this.revealer.hide({direction: this.DOM.el.dataset.direction || 'rtl', delay: this.DOM.el.dataset.delay || 0}) : this.revealer.hide();
+		}
+		showFilled() {
+			return this.revealer.showFilled({direction: this.DOM.el.dataset.direction || 'rtl', delay: this.DOM.el.dataset.delay || 0});
+		}
+		hideFilled() {
+			return this.revealer.hideFilled({direction: this.DOM.el.dataset.direction || 'rtl', delay: this.DOM.el.dataset.delay || 0});
+		}
+		setTransform(transform) {
+			const dist = distance(this.itemCenter.x, this.itemCenter.y, center.x, center.y);
+			const tx = transform.translateX/win.width*dist || 0;
+			const ty = transform.translateY/win.height*dist || 0;
+			this.DOM.el.style.transform = `translate3d(${tx}px, ${ty}px, 0)`;
+		}
+		isNavCtrl() {
+			return this.DOM.el.classList.contains('grid__item--nav');
+		}
+	};
 
-    // Preload images
-    const preloadImages = () => {
-        return new Promise((resolve, reject) => {
-            imagesLoaded(document.querySelectorAll('.content__item-img'), {background: true}, resolve);
-        });
-    };
-    
-    // And then..
-    preloadImages().then(() => {
-        // Remove the loader
-        document.body.classList.remove('loading');
-        // Get the scroll position and update the lastScroll variable
-        getPageYScroll();
-        lastScroll = docScroll;
-        // Initialize the Smooth Scrolling
-        new SmoothScroll();
-    });
-}
+	class Grid {
+		constructor(el, options) {
+			this.CONFIG = {
+				filledColor: '#fff'
+			};
+			Object.assign(this.CONFIG, options);
+			this.DOM = {};
+			this.DOM.el = el;
+			this.DOM.items = Array.from(this.DOM.el.querySelectorAll('div.grid__item'));
+			this.DOM.name = this.DOM.el.querySelector('.grid__item--name');
+			this.DOM.title = this.DOM.el.querySelector('.grid__item--title');
+			this.DOM.text = this.DOM.el.querySelector('.grid__item--text');
+			this.textElems = [this.DOM.name, this.DOM.title, this.DOM.text];
+			this.layout();
+		}
+		layout() {
+			this.itemsTotal = this.DOM.items.length;
+			this.items = [];
+			this.DOM.items.forEach((item) => this.items.push(new GridItem(item, {filledColor: this.CONFIG.filledColor})));
+		}
+		show(filled = false, animation = true) {
+			return new Promise((resolve, reject) => {
+				this.DOM.el.classList.add('grid--animating');
+				this.hideItems();
+
+				this.DOM.el.classList.add('grid--current');
+				const promises = [];
+				for (let i = 0; i < this.itemsTotal; i++) {
+					const promise = filled ? this.items[i].showFilled() : this.items[i].show(animation);
+					promises.push(promise);
+				};
+				for (let i = 0, len = this.textElems.length; i < len; i++) {
+					const promise = this.animateText(this.textElems[i], 'In');
+					promises.push(promise);
+				};
+				Promise.all(promises).then(() => {
+					this.resetTextClasses('In');
+					this.DOM.el.classList.remove('grid--animating');
+					resolve()
+				});
+			});
+		}
+		hide(filled = false, animation = true) {
+			return new Promise((resolve, reject) => {
+				this.DOM.el.classList.add('grid--animating');
+				const promises = [];
+				for (let i = 0; i < this.itemsTotal; i++) {
+					const promise = filled ? this.items[i].hideFilled() : this.items[i].hide(animation);
+					promises.push(promise);
+				};
+				for (let i = 0, len = this.textElems.length; i < len; i++) {
+					const promise = this.animateText(this.textElems[i], 'Out');
+					promises.push(promise);
+				};
+				Promise.all(promises).then(() => {
+					this.resetTextClasses('Out');
+					this.DOM.el.classList.remove('grid--animating');
+					this.DOM.el.classList.remove('grid--current');
+					resolve();
+				});
+			});
+		}
+		animateText(el, dir) {
+			return new Promise((resolve, reject) => {
+				el.classList.add(`grid__item--animate${dir}`);
+				el.addEventListener('animationend', resolve);
+			});
+		}
+		resetTextClasses(dir) {
+			for (let i = 0, len = this.textElems.length; i < len; i++) {
+				this.textElems[i].classList.remove(`grid__item--animate${dir}`);
+			};
+		}
+		hideItems() {
+			for (let i = 0; i < this.itemsTotal; i++) {
+				this.items[i].hide(false);
+			};
+		}
+		tilt(transform) {
+			for (let i = 0; i < this.itemsTotal; i++) {
+				const item = this.items[i];
+				if ( !item.isNavCtrl() ) {
+					item.setTransform(transform);
+				}
+			};
+		}
+	};
+
+	class Slideshow {
+		constructor(grids, options) {
+			this.CONFIG = {
+				filledColor: false, // false || colorvalue (e.g. '#666')
+				hasTilt: false,
+				tilt: {maxTranslationX: 50, maxTranslationY: 50}
+			};
+			Object.assign(this.CONFIG, options);
+			this.DOM = {};
+			this.DOM.grids = grids;
+			this.init();
+		}
+		init() {
+			this.current = 0;
+			this.gridsTotal = this.DOM.grids.length;
+			this.grids = [];
+			this.DOM.grids.forEach((grid) => this.grids.push(new Grid(grid, {
+				filledColor: this.CONFIG.filledColor
+			})));
+			this.initEvents();
+		}
+		initEvents() {
+			Array.from(document.querySelectorAll('.grid__item--nav-next')).forEach((ctrl) => ctrl.addEventListener('click', (ev) => this.navigate(ev, 'next')));
+			Array.from(document.querySelectorAll('.grid__item--nav-prev')).forEach((ctrl) => ctrl.addEventListener('click', (ev) => this.navigate(ev, 'prev')))
+			if ( this.CONFIG.hasTilt ) {
+				document.addEventListener('mousemove', (ev) => this.onmousemove(ev));
+				window.addEventListener('resize', (ev) => debounce(this.onresize()));
+			}
+		}
+		onmousemove(ev) {
+			requestAnimationFrame(() => {
+				const mousepos = getMousePos(ev);
+				const transX = 2*this.CONFIG.tilt.maxTranslationX/win.width*mousepos.x - this.CONFIG.tilt.maxTranslationX;
+				const transY = 2*this.CONFIG.tilt.maxTranslationY/win.height*mousepos.y - this.CONFIG.tilt.maxTranslationY;
+				this.grids[this.current].tilt({translateX: transX, translateY: transY});
+			});	
+		}
+		onresize(ev) {
+			win = {width: window.innerWidth, height: window.innerHeight};
+			center = {x: win.width/2, y: win.height/2};
+		}
+		navigate(ev, direction) {
+			if ( this.isAnimating ) {
+				return false;
+			} 
+			this.isAnimating = true;
+			const currentGrid = this.grids[this.current];
+			this.current = direction === 'next' ? (this.current < this.gridsTotal-1 ? this.current+1 : 0) : (this.current > 0 ? this.current-1 : this.gridsTotal-1);
+			const nextGrid = this.grids[this.current];
+			const filled = this.CONFIG.filledColor;
+			currentGrid.hide(!!filled).then(() => {
+				nextGrid.show(!!filled).then(() => this.isAnimating = false);
+				if ( this.CONFIG.hasTilt ) {
+					this.onmousemove(ev);
+				}
+			});
+		}
+	};
+
+	window.Slideshow = Slideshow;
+};
