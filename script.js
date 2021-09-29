639edf3a14fa672fb24e9b691eedf8194a0e8ac2
marc <marc@rromana.com>
2021-09-29 13:05:28 -0700

    test

---
+++ script.js
@@ -0,0 +1,11 @@
+// Initialize the Locomotive scroll
+   var scroll = new LocomotiveScroll({
+     el: document.querySelector('[data-scroll-container]'),
+     smooth: true,
+     smartphone: {
+        smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
+});
